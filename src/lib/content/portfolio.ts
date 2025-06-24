cb57781347392dc16dca356a8d1bd351b280d97c
Nitin Kumar Singh <dear.nk.comrade@gmail.com>
2025-06-24 11:51:58 +0530

    fixed role

--- src/lib/content/portfolio.ts
+++ src/lib/content/portfolio.ts
@@ -19,9 +19,9 @@ export const author = {
 };
 
 export const seoData = {
-  title: 'Nitin Kumar Singh | Front-End Developer',
+  title: 'Nitin Kumar Singh | Full Stack Developer',
   description:
-    'Nitin Kumar Singh is a front-end developer who specializes in building (and occasionally designing) exceptional visual interfaces.',
+    'Nitin Kumar Singh is a full stack developer who specializes in building (and occasionally designing) exceptional visual interfaces.',
   author: author.name,
   image:
     'https://user-images.githubusercontent.com/68834718/217467445-0b0d2d63-f8ad-4702-8474-814eb2cbb3be.png',
